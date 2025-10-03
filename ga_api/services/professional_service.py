16e1ccf9587035bb3dc495afeb1fc674f6400095
Pedro <revan271205@gmail.com>
2025-10-03 17:00:54 -0300

    commit 3/10/2025

--- ga_api/services/professional_service.py
+++ ga_api/services/professional_service.py
@@ -1,3 +1,5 @@
+# ga_api/services/professional_service.py
+
 import uuid
 from datetime import datetime
 from typing import List
@@ -6,6 +8,7 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ga_api.db.dao.professional_dao import ProfessionalDAO
+from ga_api.db.dao.speciality_dao import SpecialityDAO
 from ga_api.db.dependencies import get_db_session
 from ga_api.db.models.professionals_model import Professional
 from ga_api.db.models.users import User
@@ -22,6 +25,7 @@ class ProfessionalService:
     def __init__(self, session: AsyncSession = Depends(get_db_session)) -> None:
         self.session = session
         self.professional_dao = ProfessionalDAO(session)
+        self.speciality_dao = SpecialityDAO(session)
 
     async def create_professional(
         self,
@@ -30,7 +34,26 @@ class ProfessionalService:
     ) -> Professional:
         AdminUtils.validate_user_is_admin(admin_user)
 
-        new_professional = Professional(**professional_create_request.model_dump())
+        professional_data = professional_create_request.model_dump()
+        speciality_ids = professional_data.pop("specialities", [])
+
+        new_professional = Professional(**professional_data)
+
+        if speciality_ids:
+            specialities = []
+            for speciality_id in speciality_ids:
+                if speciality_id:
+                    speciality = await self.speciality_dao.find_by_id(speciality_id)
+                    if speciality:
+                        specialities.append(speciality)
+                    else:
+                        raise HTTPException(
+                            status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Especialidade com id "
+                            f"{speciality_id} não encontrada.",
+                        )
+            new_professional.specialities = specialities
+
         AdminUtils.populate_admin_data(new_professional, admin_user)
         return await self.professional_dao.save(new_professional)
 
