023346a7ea2b869dcce237fd5d5620d1eeb0b4aa
Pedro Henrique Bavaresco dos Santos <revan271205@gmail.com>
2025-10-16 17:20:46 -0300

    Refactor UserDAO by removing unused code
    Removed unused imports and cleaned up the UserDAO class.

--- ga_api/db/dao/user_dao.py
+++ ga_api/db/dao/user_dao.py
@@ -5,27 +5,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from ga_api.db.dao.abstract_dao import AbstractDAO
 from ga_api.db.dependencies import get_db_session
 from ga_api.db.models.users import User
-
-
-class UserDAO(AbstractDAO[User]):
-    def __init__(self, session: AsyncSession = Depends(get_db_session)) -> None:
-        super().__init__(User, session)
-
-    async def find_by_email(self, email: str) -> User | None:
-        result = await self._session.execute(
-            select(User).where(User.email == email),  # type: ignore
-        )
-        return result.scalar_one_or_none()
-
-
-    from typing import List
-from fastapi import Depends
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from ga_api.db.dao.abstract_dao import AbstractDAO
-from ga_api.db.dependencies import get_db_session
-from ga_api.db.models.users import User
 from ga_api.enums.user_role import UserRole
 
 
